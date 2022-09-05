241d2d653885a83d1cfecd870017806dcc55bfa5
LuanBraga <luanbraga.lb@gmail.com>
2022-09-05 14:45:06 -0300

    Add log.info on User Service methods

--- src/main/java/io/getarrays/userservice/domain/Role.java
+++ src/main/java/io/getarrays/userservice/domain/Role.java
@@ -5,10 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class Role {
-    @Id
+    @Id @GeneratedValue(strategy = AUTO)
     private Long id;
+
+    private String name;
 }
